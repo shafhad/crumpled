0afee3e67f98698f2681af6fd0a2154bb114fb4d
shafhad <66770462+shafhad@users.noreply.github.com>
2020-12-18 20:27:16 +0530

    Update Paper.Ball.js

--- Paper.Ball.js
+++ Paper.Ball.js
@@ -7,9 +7,9 @@ class Ball {
           density:1.2
       }
 
-      this.body = Bodies.circle(this.x, this.y, this.r/2, options);
+     
       this.image = loadImage("paper.png");
-      
+       this.body = Bodies.circle(this.x, this.y, this.r/2, options);
       World.add(world, this.body);
     }
     display(){
